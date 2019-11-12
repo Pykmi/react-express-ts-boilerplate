af94d410c68888c95642e00ecb09ba45c3b932e0
Tomi Kaistila <tomi.kaistila@gofore.com>
2019-11-12 13:43:36 +0200

    Refined the cors middleware.

--- server/middleware/cors.middleware.ts
+++ server/middleware/cors.middleware.ts
@@ -1,18 +1,17 @@
 import { NextFunction, Request, Response } from "express";
 
-// CORS middleware
+const headers = {
+    allowOrigin: { field: "Access-Control-Allow-Origin", value: "*" },
+    allowHeaders: { field: "Access-Control-Allow-Headers", value: "Origin, X-Requested-With, Content-Type, Accept" },
+    options: { field: "Access-Control-Allow-Methods", value: "GET, DELETE, PATCH, POST, PUT" }
+};
+
 const cors = (req: Request, res: Response, next: NextFunction) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
+    res.header(headers.allowOrigin.field, headers.allowOrigin.value);
+    res.header(headers.allowHeaders.field, headers.allowHeaders.value);
 
     if(req.method === "OPTIONS") {
-        res.header(
-            "Access-Control-Allow-Methods",
-            "GET, DELETE, PATCH, POST, PUT"
-        );
+        res.header(headers.options.field, headers.options.value);
         return res.status(200).json({});
     }
 
